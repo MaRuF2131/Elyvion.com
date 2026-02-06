b0bb21c6677c50a4f083b50856aa89f9ff1ec396
Jannatul <limajannat1675@gmail.com>
2026-02-06 21:39:46 +0600

    added polishing to service banner section

--- app/(user)/services/ServiceSection.js
+++ app/(user)/services/ServiceSection.js
@@ -4,21 +4,28 @@ import Image from "next/image";
 
 export default function ServiceSection() {
   return (
-    <section className="w-full py-10 md:py-12 px-3 md:px-10 lg:px-28 bg-white md:mt-24">
-      <div className="w-full mx-auto grid md:grid-cols-2 gap-4 md:gap-8 items-stretch">
+    <section className="w-full py-10 md:py-16 px-4 md:px-10 lg:px-28 bg-white mt-20 md:mt-20">
+      <div className="w-full mx-auto grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
 
-        {/* Left Side */}
-        <div className="relative flex items-end rounded-3xl bg-[#0f5366] overflow-hidden aspect-square md:aspect-auto md:min-h-[500px] lg:min-h-[600px] group">
-          <div className="absolute space-y-6 p-6 md:p-12 md:pb-10 md:px-6 text-white">
-            <h2 className="text-2xl sm:text-4xl md:text-5xl font-bold md:pl-10 md:mb-4 mb-3">How we help</h2>
-            <p className="text-sm md:text-base text-white/90 md:pb-10 md:pl-10 md:pr-7 leading-relaxed">
-              We specialise in Social, Paid, Creative, Influencer and Strategy and work with fast-growth brands and household names across the globe.
+        {/* Left Card */}
+        <div className="relative rounded-3xl bg-[#0f5366] overflow-hidden aspect-square min-h-[300px] md:aspect-auto  md:min-h-[500px] lg:min-h-[600px] flex items-center md:items-end">
+
+          {/* Content */}
+          <div className="text-white px-6 pb-8 md:px-14 md:pb-20">
+            <h2 className="text-2xl sm:text-4xl md:text-5xl font-bold mb-4">
+              How we help
+            </h2>
+
+            <p className="text-sm md:text-base text-white/90 leading-relaxed">
+              We specialise in Social, Paid, Creative, Influencer and Strategy
+              and work with fast-growth brands and household names across the globe.
             </p>
           </div>
+
         </div>
 
-        {/* Right Side */}
-        <div className="relative rounded-3xl overflow-hidden aspect-square md:aspect-auto md:min-h-[500px] lg:min-h-[600px] group">
+        {/* Right Card */}
+        <div className="relative rounded-3xl overflow-hidden aspect-square md:aspect-auto min-h-[300px] md:min-h-[500px] lg:min-h-[600px]">
           <Image
             src="/images/Best-Large-Social-Agency-AgJAdqlO.webp"
             alt="Service Right"
@@ -28,6 +35,7 @@ export default function ServiceSection() {
             priority
           />
         </div>
+
       </div>
     </section>
   );
